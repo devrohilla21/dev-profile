a0d2ef3d6fc4897017c0ffc2029685bcb0946d12
devrohilla21 <k.devrohilla21aug@gmail.com>
2025-03-18 22:46:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-    let sections = document.querySelectorAll("section");
-    let observer = new IntersectionObserver((entries) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add("fade-in");
-            }
-        });
-    }, { threshold: 0.1 });
-
-    sections.forEach(section => {
-        observer.observe(section);
-    });
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Website Loaded!");
 });
