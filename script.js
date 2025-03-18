ac6d0c265c323ca287575955315a7b2a26303bc7
devrohilla21 <k.devrohilla21aug@gmail.com>
2025-03-18 22:58:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("Website Loaded!");
+document.addEventListener("DOMContentLoaded", function () {
+    let sections = document.querySelectorAll("section");
+
+    let observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("fade-in");
+            }
+        });
+    }, { threshold: 0.1 });
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
 });
